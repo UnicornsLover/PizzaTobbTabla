3dce522fd1d7dc38f815248c5c767b2283483f4a
István Bálint <istvann.balint@gmail.com>
2020-01-21 14:25:08 +0100

    OrderItemsView Update

--- TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/repository/OrderItemsView/RepositoryOrderItemsView.cs
+++ TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/repository/OrderItemsView/RepositoryOrderItemsView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TobbbformosPizzaAlkalmazasTobbTabla.model;
 using TobbbformosPizzaAlkalmazasTobbTabla.Model;
 
 namespace TobbbformosPizzaAlkalmazasTobbTabla.Repository
@@ -12,7 +13,7 @@ namespace TobbbformosPizzaAlkalmazasTobbTabla.Repository
         /// <summary>
         /// Rendelések tételeit tartalmazza a megjelenítéshez
         /// </summary>
-        private List<RepositoryOrderItemsView> roiv;
+        private List<OrderItemsView> roiv;
 
         /// <summary>
         /// Konstruktor, amely a rendelés száma alapján feltölti a listát
@@ -20,6 +21,18 @@ namespace TobbbformosPizzaAlkalmazasTobbTabla.Repository
         /// <param name="orderNumber">Rendelés azonosító</param>
         public RepositoryOrderItemsView(int orderNumber, List<Item> items, List<Pizza> pizzas)
         {
+            List<Item> iviews = items.FindAll(i=> i.getOrderId() == orderNumber);
+            foreach (Item i in iviews)
+            {
+                Pizza pizza = pizzas.Find(p => p.getId() == i.getPizzaId());
+                OrderItemsView oiv = new OrderItemsView(
+                    orderNumber,
+                    i.getPiece(),
+                    pizza.getNeme(),
+                    pizza.getPrice()
+                    );
+                roiv.Add(oiv);
+            }
             
         }
     }
